d2832e3d25d2e3d306f7cda0bb85f4ef3ede3c68
halimawebd <inthesumon1010@gmail.com>
2025-02-25 23:52:49 +0600

    added the addmoney

---
+++ js/cashout.js
@@ -0,0 +1,16 @@
+// document.getElementById('cashout-btn').addEventListener('click', function () {
+    
+//     const pin = document.getElementById('cashout-pin').value;
+//     const convertedPin = parseInt(pin);
+//     const amount = document.getElementById('cashout-amount').value;
+//     const convertedAmount = parseFloat(amount);
+//     const balance = document.getElementById('main-balance').innerText;
+//     const convertedsMainBalance = parseFloat(balance);
+//     if (convertedPin === 1234) {
+//         const sum = convertedsMainBalance - convertedAmount;
+//         const balance = document.getElementById('main-balance').innerText = sum;
+//     }
+//     else {
+//         console.log('enter valid pin')
+//     }
+// })
---
+++ js/toggle.js
@@ -0,0 +1,12 @@
+// document.getElementById('cashout').style.display = 'none';
+  
+// document.getElementById('add-money-box').addEventListener('click', function () {
+//     document.getElementById('cashout').style.display = 'none';
+//     document.getElementById('addmony').style.display = 'block';
+// })
+
+// document.getElementById('cashout-box').addEventListener('click', function () {
+//      document.getElementById('cashout').style.display = 'block';
+//     document.getElementById('addmony').style.display = 'none';
+// })
+
