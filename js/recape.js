e9f41d135c5fd8a44c2e29f6a40678c294fb304f
halimawebd <inthesumon1010@gmail.com>
2025-02-25 22:01:09 +0600

    first commit

--- js/recape.js
+++ js/recape.js
@@ -1,21 +1,21 @@
-// document.getElementById('add-money').addEventListener('click', function () {
-//     const pin = document.getElementById('pin').value;
+document.getElementById('add-money').addEventListener('click', function () {
+    const pin = document.getElementById('pin').value;
     
-//     const convertedPin = parseInt(pin);
-//     const amount = document.getElementById('amount').value;
-//     const convertedAmount = parseFloat(amount);
-//     const mainBalance = document.getElementById('main-balance').innerText;
-//     const convertedMainBalance = parseFloat(mainBalance);
-//     if (amount && pin) {
-//         if (convertedPin === 1234) {
-//             const sum = convertedMainBalance + convertedAmount;
-//             document.getElementById('main-balance').innerText = sum;
-//         }
-//         else {
-//             alert('enter valid number')
-//         }
-//     }
-//     else {
-//         console.log('enter number')
-//     }
-// })
+    const convertedPin = parseInt(pin);
+    const amount = document.getElementById('amount').value;
+    const convertedAmount = parseFloat(amount);
+    const mainBalance = document.getElementById('main-balance').innerText;
+    const convertedMainBalance = parseFloat(mainBalance);
+    if (amount && pin) {
+        if (convertedPin === 1234) {
+            const sum = convertedMainBalance + convertedAmount;
+            document.getElementById('main-balance').innerText = sum;
+        }
+        else {
+            alert('enter valid number')
+        }
+    }
+    else {
+        console.log('enter number')
+    }
+})
