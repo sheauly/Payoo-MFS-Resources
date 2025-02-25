4071ec965e0f927e9f6fba9af782dbd0cfd26ce1
halimawebd <inthesumon1010@gmail.com>
2025-02-25 21:02:17 +0600

    first commit

---
+++ js/test.js
@@ -0,0 +1,18 @@
+
+// document.getElementById('login-btn').addEventListener('click', function (event) {
+//     event.preventDefault();
+//     const acountNumber = document.getElementById('Acount-number').value;
+//     const pin = document.getElementById('pin').value;
+//     const convertedPin = parseInt(pin)
+//     if (acountNumber.length === 11) {
+//         if (convertedPin === 1234) {
+//             window.location.href ="./main.html"
+//         }
+//         else {
+//             alert('pin thik nai')
+//         }
+//     }
+//     else {
+//         alert('need valid acount number')
+//     }
+// })
