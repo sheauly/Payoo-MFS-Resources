9a95e24391ce28880ebdb3593eb85c2f6f22814b
halimawebd <inthesumon1010@gmail.com>
2025-02-25 23:54:54 +0600

    a

--- js/addMoney.js
+++ js/addMoney.js
@@ -1,17 +1,17 @@
-// document.getElementById('add-money').addEventListener('click', function (event) {
-//     event.preventDefault();
-//     const amount = document.getElementById('amount').value;
-//     const convertedAmount = parseFloat(amount);
-//     const pin = document.getElementById('pin').value;
-//     const convertedPin = parseInt(pin);
-//     const mainBalance = document.getElementById('main-balance').innerText;
-//     const convertedMainbalance = parseFloat(mainBalance);
+document.getElementById('add-money').addEventListener('click', function (event) {
+    event.preventDefault();
+    const amount = document.getElementById('amount').value;
+    const convertedAmount = parseFloat(amount);
+    const pin = document.getElementById('pin').value;
+    const convertedPin = parseInt(pin);
+    const mainBalance = document.getElementById('main-balance').innerText;
+    const convertedMainbalance = parseFloat(mainBalance);
 
-//     if (convertedPin === 1234) {
-//         const sum = convertedMainbalance + convertedAmount;
-//         const mainBalance = document.getElementById('main-balance').innerText = sum;
-//     }
-//     else {
-//         // console.log('pin sotik nai')
-//     }
-// })
+    if (convertedPin === 1234) {
+        const sum = convertedMainbalance + convertedAmount;
+        const mainBalance = document.getElementById('main-balance').innerText = sum;
+    }
+    else {
+        // console.log('pin sotik nai')
+    }
+})
